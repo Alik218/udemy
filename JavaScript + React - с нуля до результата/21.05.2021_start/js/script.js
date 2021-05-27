670f3ce6e4609da18ee7297a3c6918b7aca487cc
Vitaliy <Alik218@ukr.net>
2021-05-27 19:32:59 +0300

    27_05_21 Lessons

--- JavaScript + React - с нуля до результата/21.05.2021_start/js/script.js
+++ JavaScript + React - с нуля до результата/21.05.2021_start/js/script.js
@@ -1,28 +1,70 @@
 "use strict";
 
 
+//  // Урок 12 Условия
+
+
+// if (4 == 9) {
+//     console.log('ok!');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 38;
+
+// if (num < 49) {
+//     console.log('Eroor');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('ok');
+// }
+
+
+// (num === 50) ? console.log('ok') : console.log('Eroor');
+
+
+// switch (true) {
+//     case num < 49:
+//       console.log('error 49');
+//       break;
+
+//     case num == 50:
+//       console.log('ok');
+//       break;
+
+//     case num < 100:
+//       console.log('error 100');
+//       break;
+
+//     default:
+//       console.log('не в етот раз');
+//       break;
+// }
+
+
 //  // Урок 11 Практика ч.1
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = +b;
-personalMovieDB.movies[c] = +d;
+// personalMovieDB.movies[a] = +b;
+// personalMovieDB.movies[c] = +d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 
 //  // Урок 10 GitHub
