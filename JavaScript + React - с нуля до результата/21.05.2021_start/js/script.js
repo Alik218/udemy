fb2a37dbb989ec667df7416cfe670b5acc7f23fc
Vitaliy <Alik218@ukr.net>
2021-05-27 18:57:58 +0300

    27_05_21 Practice1

---
+++ JavaScript + React - с нуля до результата/21.05.2021_start/js/script.js
@@ -0,0 +1,76 @@
+"use strict";
+
+
+//  // Урок 11 Практика ч.1
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = +b;
+personalMovieDB.movies[c] = +d;
+
+console.log(personalMovieDB);
+
+
+//  // Урок 10 GitHub
+
+
+// // Урок 9 Операторы в JS
+
+
+// let incr = 10;
+// let decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+  
+
+// // Урок 8 Интерполяция (ES6)
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Vitaliy';
+
+// alert(`Hello, ${user}`);
+
+
+// // Урок 7 Простое общения с пользователем
+
+
+// alert('hello wold');
+
+// const result = confirm('Are you heree?');
+// console.log(result);
+
+// const answer = prompt('You heaw 18?','18');
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как ваше фамилия?', '');
+// answers[2] = prompt('лет скока а?', '');
+
+// document.write(answers);
+
+
