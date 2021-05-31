50289bc4c926e863dfc2b4a659c8e8aee08c64d0
Vitaliy <Alik218@ukr.net>
2021-05-31 19:51:44 +0300

    practic_3

--- JavaScript + React - с нуля до результата/21.05.2021_start/js/script.js
+++ JavaScript + React - с нуля до результата/21.05.2021_start/js/script.js
@@ -1,5 +1,241 @@
 "use strict";
 
+//  // Практика ч.3
+
+
+let numberOfFilms;
+  
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+
+start();
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+function rememberMyFilsm() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+  
+  
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = +b;
+      console.log('ok');
+    }
+    else {
+      console.log('error');
+      i--;
+    }
+  }
+}
+
+rememberMyFilsm();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+  } else {
+    console.log('error');
+  }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+    personalMovieDB.genres[i - 1] = genre;
+  }
+}
+
+writeYourGenres();
+
+console.log(personalMovieDB);  
+
+
+//  // урок 17 Методы и свойства строк и чисел
+
+
+// const str = 'test';
+// console.log(str.toLowerCase());
+// console.log(str);
+
+// const fruit = 'Some fruit';
+// console.log(fruit.indexOf('fruit'));
+
+// const logg = 'Hello world';
+// console.log(logg.slice(6, 11));
+// console.log(logg.substring(6, 11));
+// console.log(logg.substr(6, 3));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = '12.2px';
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+
+//  // урок 16 Function
+
+
+// let num = 20;
+
+// function showFirstMessage (text) {
+//   console.log(text);
+//   num = 10;
+//   console.log(num);
+// }
+
+// showFirstMessage('Hello World');
+// console.log(num);
+
+// function calc(a, b) {
+//   return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 10));
+// console.log(calc(12, 23));
+
+
+// function ret() {
+//   let num = 50;
+//   return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// const logger = function() {
+//   console.log('Hello');
+// };
+
+// logger();
+
+
+// const calc = (a, b) => {
+//   console.log('1');
+//   return a + b;
+// };
+
+
+//  // урок 14 Практика ч2
+
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt('Один из последних просмотренных фильмов?', ''),
+//   b = prompt('На сколько оцените его?', '');
+
+
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = +b;
+//     console.log('ok');
+//   }
+//   else {
+//     console.log('error');
+//     i--;
+//   }
+// }
+
+//  // вариант 1
+// switch (true) {
+//   case personalMovieDB.count < 10:
+//     console.log('Просмотрено довольно мало фильмов');
+//     break;
+
+//   case personalMovieDB.count > 10 && personalMovieDB.count < 30:
+//     console.log('Вы классический зрител');
+//     break;
+
+//   case personalMovieDB.count >= 30:
+//     console.log('Вы киноман');
+//     break;
+
+//   default:
+//     console.log('error');
+//     break;
+// }
+
+//  // вариант 2 
+// if (personalMovieDB.count < 10) {
+//   console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//   console.log('Вы классический зритель');
+// } else if (personalMovieDB.count >= 30) {
+//   console.log('Вы киноман');
+// } else {
+//   console.log('error');
+// }
+
+
+// console.log(personalMovieDB);  
+
+
+//  // Урок 13 Циклы
+
+
+// let num = 50;
+
+// while (num < 55) {
+//   console.log(num);
+//   num++;
+// }
+
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//   console.log(num);
+//   num++;
+// }
+
+// for (let i = 1; i < 10; i++) {
+//   if (i === 6) {
+//     // break;
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
 
 //  // Урок 12 Условия
 
